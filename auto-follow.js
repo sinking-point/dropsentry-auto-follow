057112f8a7a67757c3ec21d06930c81bdc22a5f0
William Jack Bradley <billy.jack.bradley@gmail.com>
2021-04-06 16:26:57 +0100

    Preserve DropSentry referrer

--- auto-follow.js
+++ auto-follow.js
@@ -4,5 +4,9 @@
     let encoded_url = percent_encoded.substring(percent_encoded.indexOf("http"));
     let decoded = decodeURIComponent(encoded_url);
     let url = decoded.substring(0, decoded.indexOf('"'));
-    window.location.href = url;
+
+    let a = document.createElement("a");
+    a.setAttribute("href", url);
+    document.body.appendChild(a);
+    a.click();
 })();
